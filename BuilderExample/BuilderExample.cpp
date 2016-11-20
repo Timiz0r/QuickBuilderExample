6f066c4715666cd01313512d53e431fdaa1ed41c
Tim Marro <timiz0r@hungry.codes>
2016-11-19 19:51:58 -0800

    That bit of code was for a sanity check, since a past error message was kinda unusual to a C++ plebe

--- BuilderExample/BuilderExample.cpp
+++ BuilderExample/BuilderExample.cpp
@@ -5,9 +5,6 @@
 
 int main()
 {
-	std::string s1 = "Test";
-	std::string&& r3 = s1 + s1;
-
 	Incrementer::Builder builder;
 	Incrementer incrementer{ builder.SetIncrement(5).SetStart(2).Build() };
 
